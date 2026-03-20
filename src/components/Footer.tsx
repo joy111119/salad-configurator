2cd43e023d157c1ec4801981fa3578639399e3be
PrabeshShrestha786 <prabesh.shrestha786@gmail.com>
2026-03-20 11:40:43 +0200

    adds footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+function Footer() {
+  return (
+    <footer className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
+      
+      <div>
+        <h2 className="font-bold mb-2">Contact</h2>
+        <p>Email: info@freshfood.com</p>
+        <p>Phone: +123 456 789</p>
+      </div>
+
+      <div>
+        <h2 className="font-bold mb-2">Social Media</h2>
+        <p>Instagram</p>
+        <p>Facebook</p>
+        <p>Twitter</p>
+      </div>
+
+      <div>
+        <h2 className="font-bold mb-2">Branding</h2>
+        <p>Fresh Food Factory</p>
+        <p>FRESSE</p>
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer
