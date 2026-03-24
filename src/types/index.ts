66f53359515c212af1fbe7c16d94bb32a830a1de
Prabesh Shrestha <prabesh.shrestha786@gmail.com>
2026-03-24 15:08:38 +0200

    Add PriceListItem interface to types

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+// BaseType interface
+export interface BaseType {
+  id: number
+  name: string
+  price?: number
+  image_url: string
+  barcode_url: string
+}
+
+// Bowl interface extending BaseType
+export interface Bowl extends BaseType {
+  base_type_id?: number
+  volume?: number
+  slot_count: number
+  shape: 'round' | 'square'
+}
+
+//PriceListItem interface
+export interface PriceListItem {
+  id: number
+  item_id: number
+  price: number
+  type?: string
+}
