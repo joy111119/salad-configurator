28ecfd64cb38d4a51b54e0d00a124cb0c99005c0
Saharsh Verma <saharshvermaxb@gmail.com>
2026-03-28 18:22:26 +0200

    Added BaseSelection on the right panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Header from './components/Header'
+import  BaseSelection  from './components/BaseSelection'
 
 function App() {
   return (
-    <div>
+    <div className="p-6 space-y-6">
       <Header />
+
+      <div className="flex justify-end">
+        <BaseSelection />
+      </div>
     </div>
   )
 }
