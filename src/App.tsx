4963414d56bb8be19b55b4eb8618c2edd21bdf68
Saharsh Verma <saharshvermaxb@gmail.com>
2026-03-28 15:46:57 +0200

    Added the Bowl Selection and imported Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Header from './components/Header'
+import BowlSelection from './components/BowlSelection'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Header />
+
+      <main className="p-6">
+        <BowlSelection />
+      </main>
+
+      <Footer />
     </div>
   )
 }
